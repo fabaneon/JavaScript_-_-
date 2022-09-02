a61fd0a4d5f1204438afa0906556776f983bba3e
Fabaneon <fabaneon@gmail.com>
2022-09-02 16:58:15 +0900

    2022-09-02 함수와 외부 코드 불러오기
    처음으로 처음으로 사용자로부터 입력값을 받아 연산 후 다시 출력해보았다.
    이제 좀 뭔가 실용적인 내용을 배우는 기분

---
+++ Js_5/Js_5.js
@@ -0,0 +1,7 @@
+
+function sum(){
+  let n1 = document.getElementById('n1').value;
+  let n2 = document.getElementById('n2').value;
+  let result = Number(n1) + Number(n2) ;
+  document.getElementById("result").innerHTML = result ;
+}
