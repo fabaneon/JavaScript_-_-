511a64048a23a2131ecd2e1ed9eccfa49442423a
fabaneon <fabaneon@gmail.com>
2022-09-04 23:43:46 +0900

    2022-09-04 React 공부중 추가된 내용
    지난번 미완성했던 Meberlist 걸러주는 기능이다.

--- Js_6/Js_6_MemberList.js
+++ Js_6/Js_6_MemberList.js
@@ -12,23 +12,36 @@ function memeberlistprinter(){
 // 객체에서 활용되는 배열 속에는 id값이 중복사용이 불가능하다.
 // 만일 중복되었을 경우 출력함수는 중복된 id값중 가장 최근에 추가된 값을 불러온다.
 // Ex) 위 내용에서 A_Team 이라는 id를 불러오면 A_Team: A-3가 출력되고 나머진 무시된다.
-var memberlist = {
-  "A_Team":"Fabaneon",
-  "A_Team2":"NejukMaster",
-  "A_Team3":"2001GopnikGuy",
-  "A_Team4":"Tanso5607",
-  "B_Team1":"Denver",
-  "B_Team2":"GodorHuman",
-  "B_Team3":"Clonejhs"
-};
+  let memberlist = [
+    {id: 1, team:"A_team", name:  "Fabaneon"},
+    {id: 2, team:"A_team", name:  "NejukMaster"},
+    {id: 3, team:"A_team", name:  "2001GopnikGuy"},
+    {id: 4, team:"A_team", name:  "Tanso5607"},
+    {id: 5, team:"B_team", name:  "Denver"},
+    {id: 6, team:"B_team", name:  "GodorHuman"},
+    {id: 7, team:"B_team", name:  "Clonejhs"}
+  ];
 
-  var i = 0;
-  var print = key+' : ' + memberlist[key] + "<br>";
-  document.write(print);
-  for( var key in memberlist){
-    document.getElementById('#memeberlist').innerHTML = print;
-    i++;
+  console.log(memberlist);
+  let lis = '';
+  for (var i = 0; i < memberlist.length; i++){
+    lis += "<li>" + memberlist[i].team + ' : ' + memberlist[i].name + "</li>" ;
   }
+  console.log(lis);
+  document.write(lis);
+  //
+  //
+  // let lisA = '';
+  //
+  // for (var i = 0; i < memberlist.length; i++){
+  //   if (memberlist[i].team ==="A_team") {
+  //     lisA += "<li>" + memberlist[i].team + ' : ' + memberlist[i].name + "</li>" ;
+  //   }
+  //   else {}
+  // }
+  // document.write(lisA);
+  // console.log("lisA : " + lisA);
 }
+
 // 시간관계상 아직 미완성
 // 객체 활용에 대한 심화 학습이 필요할것같다.
